34ecbe499df0f737eb57994334055f324735119c
s26802 <marceli.chlebowski@gmail.com>
2024-03-12 15:18:41 +0100

    commit Message

--- firstapp/firstapp/Program.cs
+++ firstapp/firstapp/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+static double Adavg(int[] numbers) {
+    double sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+
+    return sum / numbers.Length;
+}
+int[] arr = {1,2,3,4,};
+double average = Adavg(arr);
+Console.WriteLine(average);
