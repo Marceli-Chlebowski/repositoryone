a1f1722760fe1d11c6883e3cd0a311337084c84a
Marceli <129780388+Marceli-Chlebowski@users.noreply.github.com>
2024-03-17 14:17:00 +0100

    Update Program.cs

--- firstapp/firstapp/Program.cs
+++ firstapp/firstapp/Program.cs
@@ -11,6 +11,23 @@ static double Adavg(int[] numbers) {
 
     return sum / numbers.Length;
 }
+
+static int biggest(int[]numbers)
+{
+    int biggestint = numbers[0];
+    for(int i = 0 ; i < numbers.Length ; i++)
+    {
+        if (biggestint < numbers[i])
+            biggestint = numbers[i];
+    }
+    
+    
+    return biggestint;
+}
+
 int[] arr = {1,2,3,4,};
 double average = Adavg(arr);
 Console.WriteLine(average);
+int[] numbers = { 1, 2, 3, 4, 5, 6 };
+int biggestint = biggest(numbers);
+Console.WriteLine(biggestint);
